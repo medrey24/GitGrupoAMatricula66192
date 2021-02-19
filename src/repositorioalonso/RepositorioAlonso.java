2f6548b365037608498cc72ec89bc45062d13101
acs <acs@win10>
2021-02-19 17:04:08 -0600

    Mi primer commit.

---
+++ src/repositorioalonso/RepositorioAlonso.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorioalonso;
+
+/**
+ *
+ * @author acs
+ */
+public class RepositorioAlonso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String nombre= "Alonso Medina";
+        String matricula= "66192";
+        System.out.println(" SOY " + nombre + "Y MI MATRICULA ES" + matricula );
+        
+       
+    }
+    
+}
